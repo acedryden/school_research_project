0288b12e4489e1dc503d0d025e058d9e079fe11c
AleMori22 <146478864+AleMori22@users.noreply.github.com>
2024-02-23 13:34:31 -0500

    Pushing app structure
    set up the app structure for the first plot test

---
+++ Starter/webapp/app.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+from flask import Flask, jsonify, render_template
+
+import pandas as pd
+
+app = Flask(__name__)
+
+test_file = "D:\School UFT\school_research_project\Starter\webapp\school_location_data.csv"
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route("/api/v1.0/school_locations")
+def School_data():
+
+    #engine = create_engine("sqlite:///dow.sqlite",echo=False)
+    #Base = automap_base()
+    #Base.prepere(autoload_with = engine)
+    #Dow = Base.classes.dow
+
+    #session = Session(engine)
+    #result = session.query(dow).all()
+    #session.close()
+
+    School_locations_df = pd.read_csv(test_file)
+    data = School_locations_df.to_dict(orient = "records") 
+
+    return jsonify({"data" : data})
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)

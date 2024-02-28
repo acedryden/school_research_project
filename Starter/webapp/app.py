fdbe5ee557f20585ada7dd09899b8f0cf6c0a575
AleMori22 <146478864+AleMori22@users.noreply.github.com>
2024-02-28 18:33:50 -0500

    homepage

--- Starter/webapp/app.py
+++ Starter/webapp/app.py
@@ -10,12 +10,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
-engine = create_engine("postgresql://project_3_333t_user:3LuhMTGZ77yugy4ExOkIqqROOtWMs4rE@dpg-cnbuglv79t8c73ep52q0-a.ohio-postgres.render.com/project_3_333t", echo=False)
+#engine = create_engine("postgresql://project_3_333t_user:3LuhMTGZ77yugy4ExOkIqqROOtWMs4rE@dpg-cnbuglv79t8c73ep52q0-a.ohio-postgres.render.com/project_3_333t", echo=False)
 
-Base = automap_base()
-Base.prepare(engine, reflect=True)
+#Base = automap_base()
+#Base.prepare(engine, reflect=True)
 
-School_Location = Base.classes.School_Location_2
+#School_Location = Base.classes.School_Location_2
 
 
 # Main Route
@@ -23,46 +23,8 @@ School_Location = Base.classes.School_Location_2
 @app.route('/')
 def main():
 
-    message = "Work in progres <br> Possible routes: <br> /tables <br> /map <br> /school_locations"
-    return message
+    return render_template('home.html')
 
-#Table Names Route PROVVISORY
-
-@app.route('/tables')
-def visualize_tables():
-    engine = create_engine("postgresql://project_3_333t_user:3LuhMTGZ77yugy4ExOkIqqROOtWMs4rE@dpg-cnbuglv79t8c73ep52q0-a.ohio-postgres.render.com/project_3_333t", echo=False)
-    inspector = inspect(engine)
-    table_names = inspector.get_table_names()
-
-    tables = {}
-    for table_name in table_names:
-        columns = inspector.get_columns(table_name)
-        tables[table_name] = [column["name"] for column in columns]
-
-    return jsonify(tables)
-
-#School Locations Route
-
-@app.route('/school_locations')
-def get_school_locations():
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    results = session.query(School_Location).all()
-    print(results)
-
-    school_data = []
-
-    for result in results:
-        school_data.append({
-            "school_name": result.School_Number,
-            "latitude": result.Latitude,
-            "longitude": result.Longitude
-        })
-
-    session.close()
-
-    return jsonify(school_data)
     
 # Map Route
 

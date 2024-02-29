c2d1e4c0bfda9207e7ada3b78f5ea54ef9767411
Khemaka14 <khemakaoo@gmail.com>
2024-02-29 02:06:23 -0500

    Major Update
    Updated the API Paths, Added more data, Cleaned up the Render Server, updated html files.

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from flask import Flask, jsonify, render_template
 engine = create_engine("postgresql://project_3_333t_user:3LuhMTGZ77yugy4ExOkIqqROOtWMs4rE@dpg-cnbuglv79t8c73ep52q0-a.ohio-postgres.render.com/project_3_333t", echo=False)
 Base = automap_base()
 Base.prepare(autoload_with=engine)
-elementary = Base.classes.Elementary_School_Enrolment
+elementary = Base.classes.Enrollment
+school_info = Base.classes.School_info
 app = Flask(__name__)
 
 
@@ -21,15 +22,34 @@ def website():
     return render_template('index.html')
 
 
+@app.route("/idk")
+def school_map():
+    session = Session(engine)
+
+    school_lat = session.query(school_info.Latitude).all()
+    school_long = session.query(school_info.Longitude).all()
+
+    school_lat_list = [float(row[0]) for row in school_lat]
+    school_long_list = [float(row[0]) for row in school_long]
+
+    data = {"Latitude" : school_lat_list, "Longitude" : school_long_list}
+
+    session.close()
+    return jsonify(data)
+
 @app.route("/v0")
 def welcome():
     session = Session(engine)
+
     schoolnums = session.query(elementary.School_Number).all()
     grade8 = session.query(elementary.Grade_8_Enrolment).all()
 
     school_num = [row[0] for row in schoolnums]
     grade_8 = [row[0] for row in grade8]
+
     data = {"School_Number" : school_num, "Grade_8_Enrolment" : grade_8}
+    
+    session.close()
     return jsonify(data)
 
 @app.route("/close")
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,41 @@
 // Creating the map object
+url = "/idk"
+
+d3.json(url).then(function (response){
+  let markers = L.markerClusterGroup();
+    for (let i = 0; i < response.Longitude.length; i++) {
+      //console.log(response.Longitude[i])
+      markers.addLayer(L.marker([response.Latitude[i], response.Longitude[i]]));
+        
+    }
+    myMap.addLayer(markers);
+})
+
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+toronto = L.geoJson(point)
+ontario = L.geoJson(bounds)
+
+let baseMaps = {
+  "Street Map": street
+};
+
+
+let overlayMaps = {
+  "Toronto": toronto,
+  "Ontario": ontario
+};
+
 let myMap = L.map("map", {
     center: [43.6532, -79.3832],
-    zoom: 17
+    zoom: 17,
+    layers: [street, toronto, ontario]
   });
   
   // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+  
   
   // Use this link to get the GeoJSON data.
   function onEachFeature(feature, layer) {
@@ -17,16 +45,11 @@ let myMap = L.map("map", {
     }
 }
   // Getting our GeoJSON data
-  
-  L.geoJson(school).addTo(myMap);
-  
-    // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(point).addTo(myMap);
-  
-  
-  L.geoJSON(school, {
-    onEachFeature: onEachFeature
-}).addTo(myMap);
 
-  
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
+
+
 

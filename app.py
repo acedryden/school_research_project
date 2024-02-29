64347749278306c1776cab1efeb77e184785becf
AleMori22 <146478864+AleMori22@users.noreply.github.com>
2024-02-28 19:55:57 -0500

    mid class update

---
+++ app.py
@@ -0,0 +1,77 @@
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+from flask import Flask, jsonify, render_template
+from sqlalchemy.orm import sessionmaker
+import json
+
+
+import pandas as pd
+
+app = Flask(__name__)
+
+
+# Main Route
+
+@app.route('/')
+def main():
+
+    return render_template('home.html')
+
+
+#Table Names Route PROVVISORY
+
+@app.route('/tables')
+def visualize_tables():
+    engine = create_engine("postgresql://project_3_333t_user:3LuhMTGZ77yugy4ExOkIqqROOtWMs4rE@dpg-cnbuglv79t8c73ep52q0-a.ohio-postgres.render.com/project_3_333t", echo=False)
+    inspector = inspect(engine)
+    table_names = inspector.get_table_names()
+
+    tables = {}
+    for table_name in table_names:
+        columns = inspector.get_columns(table_name)
+        tables[table_name] = [column["name"] for column in columns]
+
+    return jsonify(tables)
+
+#School Locations Route
+
+@app.route('/api/v1.0/School_Data')
+def get_school_data():
+
+    engine = create_engine("postgresql://project_3_333t_user:3LuhMTGZ77yugy4ExOkIqqROOtWMs4rE@dpg-cnbuglv79t8c73ep52q0-a.ohio-postgres.render.com/project_3_333t", echo=False)
+
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+
+    School_Data = Base.classes.School_Location_2
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(School_Data).all()
+    print(results)
+
+    school_data = []
+
+    for result in results:
+        school_data.append({
+            "school_name": result.School_Number,
+            "latitude": result.Latitude,
+            "longitude": result.Longitude
+        })
+
+    session.close()
+
+    return jsonify(school_data)
+
+    
+# Map Route
+
+@app.route("/map")
+def map():
+    return render_template('index.html')
+
+# Debugging Function 
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ static/js/ScLoc_plot.js
@@ -0,0 +1,58 @@
+function createMap(School_location) {
+
+     // Create the tile layer that will be the background of our map.
+    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+  
+  
+     // Create a baseMaps object to hold the streetmap layer.
+     let baseMaps = {
+       "Street Map": streetmap
+     };
+  
+     // Create an overlayMaps object to hold the bikeStations layer.
+     let overlayMaps = {
+       "School Location": School_location
+     };
+  
+     // Create the map object with options.
+     let map = L.map("map-id", {
+       center: [51.2538, -85.3232],
+       zoom: 12,
+       layers: [streetmap, School_location]
+     });
+  
+     // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+     L.control.layers(baseMaps, overlayMaps, {
+       collapsed: false
+     }).addTo(map);
+}
+  
+
+    // Pull the "stations" property from response.data.
+    //let School = response.data.stations;
+  
+    // Initialize an array to hold School Markers.
+    //let schoolMarkers = [];
+  
+    // Loop through the stations array.
+    //for (let index = 0; index < stations.length; index++) {
+      //let station = stations[index];
+  
+      // For each station, create a marker, and bind a popup with the station's name.
+      //let schoolMarker = L.marker([station.lat, station.lon])
+        //.bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+  
+      // Add the marker to the schoolMarkers array.
+      //schoolMarkers.push(schoolMarker);
+    //}
+  
+    // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
+    //createMap(L.layerGroup(schoolMarkers));
+
+  
+  
+  // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
+
+  d3.json("/api/v1.0/School_Data").then(Response);

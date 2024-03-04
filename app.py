2b4a1257b224beb130d86279845fe3f099c6986f
Khemaka14 <khemakaoo@gmail.com>
2024-03-04 17:32:22 -0500

    Major Changes

--- app.py
+++ app.py
@@ -4,11 +4,23 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, text, inspect, func
 from flask import Flask, jsonify, render_template
 
+from project_pass import pas
+
+from pymongo import MongoClient
+from pprint import pprint
+import pandas as pd
+
+mongo = MongoClient(f"mongodb+srv://khemakaoo:{pas}@cluster0.f5fh96l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = mongo['Boundary']
+bounds = db["asd"]
+
 engine = create_engine("postgresql://project_3_333t_user:3LuhMTGZ77yugy4ExOkIqqROOtWMs4rE@dpg-cnbuglv79t8c73ep52q0-a.ohio-postgres.render.com/project_3_333t", echo=False)
 Base = automap_base()
 Base.prepare(autoload_with=engine)
 elementary = Base.classes.Enrollment
 school_info = Base.classes.School_info
+board_info = Base.classes.Board_info
+board_grad = Base.classes.Board_Grad
 app = Flask(__name__)
 
 
@@ -16,41 +28,75 @@ app = Flask(__name__)
 def dow_data():
     return render_template('Jumbo.html')
 
+@app.route("/api/v0/grad_rate")
+def grad_rates():
+    session = Session(engine)
+
+    Four_2017_2018 = session.query(board_grad.Four_Year_Graduation_Rate_2017_2018_Grade_9_Cohort , board_info.Municipality ,board_grad.Board_Number ).filter(board_info.Board_Number == board_grad.Board_Number).all()
+    Five_2017_2018 = session.query(board_grad.Five_Year_Graduation_Rate_2017_2018_Grade_9_Cohort , board_info.Municipality, board_grad.Board_Number).filter(board_info.Board_Number == board_grad.Board_Number).all()
+    Four_2018_2019 = session.query(board_grad.Four_Year_Graduation_Rate_2018_2019_Grade_9_Cohort , board_info.Municipality, board_grad.Board_Number).filter(board_info.Board_Number == board_grad.Board_Number).all()
+
+    data = {}
+
+    for i in range(len(Four_2017_2018)):
+        data[(Four_2017_2018[i][1]).upper()] = {}
+
+
+    for i in range(len(Four_2017_2018)):
+        data[(Four_2017_2018[i][1]).upper()][Four_2017_2018[i][2]] = {}
+
+    for i in range(len(Four_2017_2018)):
+        data[(Four_2017_2018[i][1]).upper()][Four_2017_2018[i][2]]["Four_2017_2018"] = float(Four_2017_2018[i][0])
+
+    for i in range(len(Five_2017_2018)):
+        data[(Four_2017_2018[i][1]).upper()][Five_2017_2018[i][2]]["Five_2017_2018"] = float(Five_2017_2018[i][0])
+
+    for i in range(len(Four_2018_2019)):
+        data[(Four_2017_2018[i][1]).upper()][Five_2017_2018[i][2]]["Four_2018_2019"] = float(Four_2018_2019[i][0])
+
+
+    session.close()
+    return jsonify(data)
+
+@app.route("/mongo")
+def dmongo():
+    objects = bounds.find({},{"_id":False}) 
+    return jsonify({"requests": list(objects)})
 
 @app.route("/site")
 def website():
     return render_template('index.html')
 
 
-@app.route("/idk")
-def school_map():
-    session = Session(engine)
+# @app.route("/idk")
+# def school_map():
+#     session = Session(engine)
 
-    school_lat = session.query(school_info.Latitude).all()
-    school_long = session.query(school_info.Longitude).all()
+#     school_lat = session.query(school_info.Latitude).all()
+#     school_long = session.query(school_info.Longitude).all()
 
-    school_lat_list = [float(row[0]) for row in school_lat]
-    school_long_list = [float(row[0]) for row in school_long]
+#     school_lat_list = [float(row[0]) for row in school_lat]
+#     school_long_list = [float(row[0]) for row in school_long]
 
-    data = {"Latitude" : school_lat_list, "Longitude" : school_long_list}
+#     data = {"Latitude" : school_lat_list, "Longitude" : school_long_list}
 
-    session.close()
-    return jsonify(data)
+#     session.close()
+#     return jsonify(data)
 
-@app.route("/v0")
-def welcome():
-    session = Session(engine)
+# @app.route("/v0")
+# def welcome():
+#     session = Session(engine)
 
-    schoolnums = session.query(elementary.School_Number).all()
-    grade8 = session.query(elementary.Grade_8_Enrolment).all()
+#     schoolnums = session.query(elementary.School_Number).all()
+#     grade8 = session.query(elementary.Grade_8_Enrolment).all()
 
-    school_num = [row[0] for row in schoolnums]
-    grade_8 = [row[0] for row in grade8]
+#     school_num = [row[0] for row in schoolnums]
+#     grade_8 = [row[0] for row in grade8]
 
-    data = {"School_Number" : school_num, "Grade_8_Enrolment" : grade_8}
+#     data = {"School_Number" : school_num, "Grade_8_Enrolment" : grade_8}
     
-    session.close()
-    return jsonify(data)
+#     session.close()
+#     return jsonify(data)
 
 @app.route("/close")
 def wewb():
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,55 +1,89 @@
 // Creating the map object
-url = "/idk"
-
-d3.json(url).then(function (response){
-  let markers = L.markerClusterGroup();
-    for (let i = 0; i < response.Longitude.length; i++) {
-      //console.log(response.Longitude[i])
-      markers.addLayer(L.marker([response.Latitude[i], response.Longitude[i]]));
-        
-    }
-    myMap.addLayer(markers);
-})
 
 let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
-toronto = L.geoJson(point)
-ontario = L.geoJson(bounds)
-
-let baseMaps = {
-  "Street Map": street
-};
-
-
-let overlayMaps = {
-  "Toronto": toronto,
-  "Ontario": ontario
-};
+function getColor(grad_rate){
+  if (grad_rate < 0.6){
+    return "yellow"
+  }
+  else if (grad_rate < 0.7){
+    return "orange"
+  }
+  else if (grad_rate < 0.8){
+    return "red"
+  }
+  else if (grad_rate < 0.9){
+    return "blue"
+  }
+  else if (grad_rate < 1){
+    return "green"
+  }
+}
 
 let myMap = L.map("map", {
     center: [43.6532, -79.3832],
     zoom: 17,
-    layers: [street, toronto, ontario]
+    layers: street
   });
-  
-  // Adding the tile layer
-  
-  
-  // Use this link to get the GeoJSON data.
-  function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties) {
-        layer.bindPopup(feature.properties);
-    }
-}
-  // Getting our GeoJSON data
 
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
+function optionChanged(go){
+  return go
+  }
+
+
+url_mongo = "/mongo"
+    d3.json(url_mongo).then(function (response){
+
+      url_mongo = "/api/v0/grad_rate"
+      d3.json(url_mongo).then(function (call){
 
 
 
+        function getRate(name,year){
+          for (const [key, value] of Object.entries(call)){
+              if (name == key){
+                for (const [key_1, value_1] of Object.entries(value)){
+                  return call[name][key_1][year]
+                }
+              }
+        }
+        }
 
+        L.geoJSON(response.requests[0], {
+          onEachFeature: function (feature, layer) {
+            hi = feature.properties.MUNICIPAL_NAME_SHORTFORM
+            layer.bindPopup(`<h3>${hi}</h3><hr>
+            <h4>Graduation Rate 2017-2018: ${getRate(hi,"Five_2017_2018")}</h4>
+            <h4>Graduation Rate 2017-2018: ${getRate(hi,"Four_2017_2018")}</h4>
+            <h4>Graduation Rate 2017-2018: ${getRate(hi,"Four_2018_2019")}</h4>
+            `)
+
+            layer.setStyle({color: getColor(getRate(hi,"Five_2017_2018"))}); 
+             }}).addTo(myMap)
+      
+      })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    })
+
+
+
+
+    

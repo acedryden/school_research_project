2b49413820162c827b31300fae997d4e7d61022c
Artib03 <artib.9803@gmail.com>
2024-04-23 03:00:03 -0400

    push

--- app.py
+++ app.py
@@ -49,61 +49,61 @@ Base.prepare(engine, reflect=True)
 
 @app.route('/api/v1.0/grad_model')
 
-def predicted_grad_data():#x):
+def predicted_grad_data():
 
-#     # Assign the graduations to a variable 
-    Graduation_data = Base.classes.Board_Grad_2
+    # Future_grad_data = Base.classes.future_data
+#     Graduation_Data = Base.classes.Board_Grad_2
 
 #     # Iniciate Session
-    Session = sessionmaker(bind=engine)
-    session = Session()
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
 
-    Graduation_results = session.query(Graduation_data).all()
+#     Future_grad_results = session.query(Future_grad_data).all()
+#     Graduation_results = session.query(Graduation_Data).all()
 
-#     # Extracting the data in the graduation table
-    Graduation__data = [{column.name: str(getattr(result, column.name)) for column in Graduation_data.__table__.columns} for result in Graduation_results]
+#     # Extracting the data in the Enrollment table
+#     Graduation__Data = [{column.name: str(getattr(result, column.name)) for column in Graduation_Data.__table__.columns} for result in Graduation_results]
 
+#     # Extracting the data in the Future_Enrollment table
+#     Future__grad__data = [{column.name: str(getattr(result, column.name)) for column in Future_grad_data.__table__.columns} for result in Future_grad_results]
+    
 #     # Closing Session 
-    session.close()
+#     session.close()
 
 #     # Trasform the data in to Dataframes
-    Graduation_df = pd.DataFrame(Graduation__data)
+#     Graduation_df = pd.DataFrame(Enrollment__data)
+#     Future_grad_df = pd.DataFrame(Future__Enrollment__data)
 
-    grads = np.array(list(Graduation_df))
+    model_path = "graduation_prediction_model.pkl"
 
-    grads.reshape(-1,1)
+    # Importing the ML model
+    with open(model_path, "rb") as f:
+        model = pickle.load(f)
 
-    # Need to fix this
-
-    model = pickle.load(open('graduation_prediction_model.pkl', 'rb'))
-
-    predictions = model.predict(grads)
-    print(predictions)
+        # Making predictions on the Future_Enrollment_df
+    predictions = model.predict(Future_grad_df)
 
         # Importing predictions in the Future_Enrollment_df
-    #Future_Enrollment_df["Total_Enrolment"] = predictions
-
-        # Dimentioning the predicted Total Enrolment as Integer
-    #Future_Enrollment_df["Total_Enrolment"] = Future_Enrollment_df["Total_Enrolment"].astype(int)
+    Future_grad_df["Four Year Graduation Rate 2017-2018 Grade 9 Cohort"] = predictions
 
     # Extracting the first year for each row and transforming to int
-    #Enrollment_df["Year"] = Enrollment_df["Year"].astype(str).str.split("-").str[0]    
+    Graduation_df["Year"] = Graduation_df["Year"].astype(str).str.split("-").str[0]    
 
     # Merge selected columns to Future_Enrollment_df based on School_Number
-    #Future_Enrollment_df = Future_Enrollment_df.merge(Enrollment_df[["School_Number", "School_Type", "School_Level"]], on="School_Number", how="left")
+    Future_grad_df = Future_grad_df.merge(Graduation_df[["Board Number", "Board Type", "Region"]], on="Board Number", how="left")
 
-    #Complete_df = pd.concat([Enrollment_df])
+    Complete_df = pd.concat([Graduation_df,Future_grad_df])
 
     # Selecting the data for a specific year
     #Final_df = Complete_df[Complete_df["Year"] == x]
 
-    #Final_df = Final_df.drop_duplicates(subset=["School_Number", "Year"])
+    Final_df = Final_df.drop_duplicates(subset=["Board Number", "Year"])
 
     # Convert DataFrame back to list of dictionaries
-    #Complete__Enrollment = Final_df.to_dict(orient='records')
-
+    Complete__Graduations = Final_df.to_dict(orient='records')
+    
     # Return Jsonify Board_data
-    return jsonify(predictions)
+    return jsonify(Complete_df)
 
 if __name__ == "__main__":
     app.run(debug=True)
